2eeee64e8137577fd44d4d44832c774bca18705a
Tom van den Berg <tom171296@users.noreply.github.com>
2024-01-06 21:59:24 +0000

    test commit

---
+++ src/ChainGuardian/Program.cs
@@ -0,0 +1,6 @@
+﻿using Refit;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var test = new RefitSettings().HttpMessageHandlerFactory?.Invoke() ?? new HttpClientHandler();
